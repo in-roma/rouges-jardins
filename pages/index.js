e22276a1c489d8facff738afda1f520b6b382ce9
Romain Grandjean <romain.grandjean@outlook.com>
2021-10-18 19:52:08 +0200

    layout components

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,16 @@ import styles from '../styles/Home.module.css';
 
 export default function Home(props) {
 	console.log(props.data);
-	return <Layout></Layout>;
+	return (
+		<Layout>
+			{props.data.map((el) => (
+				<div>
+					<h3>{el.title.rendered}</h3>
+					<h4>{el.excerpt.rendered}</h4>
+				</div>
+			))}
+		</Layout>
+	);
 }
 export async function getStaticProps() {
 	const res = await fetch('http://www.jcou8054.odns.fr/wp-json/wp/v2/posts');
