9c22919149fa4737c3ef7653c822654412bf880a
Romain Grandjean <romain.grandjean@outlook.com>
2021-10-20 16:32:02 +0200

    implementing graphQL tests queries from WP

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,77 @@
+import Image from 'next/image';
+import parse, { domToReact } from 'html-react-parser';
+import { GraphQLClient } from 'graphql-request';
+
 // Components
 import Layout from '../components/Layout';
 import styles from '../styles/Home.module.css';
 
-export default function Home(props) {
-	console.log(props.data);
+export default function Home({ posts }) {
+	// Options to format html tags from wordpress
+	const options = {
+		replace: ({ attribs, children }) => {
+			if (!attribs) {
+				return;
+			}
+			if (
+				attribs.class &&
+				attribs.class.includes('sqs-image-shape-container-element')
+			) {
+				return (
+					<div style={{ padding: 0 }}>
+						{domToReact(children, options)}
+					</div>
+				);
+			}
+		},
+	};
 	return (
 		<Layout>
-			{props.data.map((el) => (
-				<div>
-					<h3>{el.title.rendered}</h3>
-					<h4>{el.excerpt.rendered}</h4>
+			{posts.edges.map((el) => (
+				<div key={'div' + el.node.id}>
+					<h3 key={'title' + el.node.id}>{el.node.title}</h3>
+					<div key={'excerpt' + el.node.id}>
+						{parse(el.node.excerpt, options)}
+					</div>
+					{/* <div key={'content' + el.node.id}>
+						{parse(el.node.content, options)}
+					</div> */}
+
+					{el.node.featuredImage !== null && (
+						<img src={`${el.node.featuredImage.node.sourceUrl}`} />
+					)}
 				</div>
 			))}
 		</Layout>
 	);
 }
 export async function getStaticProps() {
-	const res = await fetch('http://www.jcou8054.odns.fr/wp-json/wp/v2/posts');
-	const data = await res.json();
+	const graphcms = new GraphQLClient(process.env.CMSWP_URL);
 
+	const { posts } = await graphcms.request(
+		`{
+			posts {
+				edges {
+				  node {
+					id
+					title
+					excerpt
+					date
+					slug
+					featuredImage {
+					  node {
+						altText
+						sourceUrl
+					  }
+					}
+				  }
+				}
+			  }
+			
+	  }`
+	);
+	console.log('data fetched:', posts.edges);
 	return {
-		props: { data },
+		props: { posts },
 	};
 }
