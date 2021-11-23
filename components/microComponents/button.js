34ce4f052e243f1bddbd5fe56cf0c0c7afcd5b77
Romain Grandjean <romain.grandjean@outlook.com>
2021-11-23 18:48:13 +0100

    stripe api webhook implementation

--- components/microComponents/button.js
+++ components/microComponents/button.js
@@ -5,10 +5,7 @@ import styles from '../../styles/components/microComponents/Button.module.scss';
 
 export default function Button({ text, onClick }) {
 	return (
-		<button
-			className={styles.buttonCart}
-			onClick={console.log('button clicked')}
-		>
+		<button className={styles.buttonCart} onClick={onClick}>
 			{text}
 		</button>
 	);
--- pages/cart.js
+++ pages/cart.js
@@ -1,13 +1,33 @@
 import Link from 'next/link';
+import axios from 'axios';
 
 // Components
 import Layout from '../components/Layout';
 import SectionBar from '../components/microComponents/sectionBar';
+import Button from '../components/microComponents/button';
 
 // Styling
 import styles from '../styles/Cart.module.scss';
 
 export default function Cart() {
+	const cartDetails = [{ id: 'price_1Jz01jG1ckaZTP2OwXpN0poe', quantity: 1 }];
+
+	const redirectToCheckout = async () => {
+		// Create Stripe checkout
+		const {
+			data: { id },
+		} = await axios.post('/api/checkout_sessions', {
+			items: Object.entries(cartDetails).map(([_, { id, quantity }]) => ({
+				price: id,
+				quantity,
+			})),
+		});
+
+		// Redirect to checkout
+		const stripe = await getStripe();
+		await stripe.redirectToCheckout({ sessionId: id });
+	};
+
 	return (
 		<Layout>
 			<div className={styles.containerCart}>
@@ -19,6 +39,7 @@ export default function Cart() {
 						</Link>
 					</div>
 				</div>
+				<Button text="Commander" onClick={redirectToCheckout} />
 			</div>
 		</Layout>
 	);
