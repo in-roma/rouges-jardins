87ea1717107b0662b0d82fe1b67d19664d3e8b1f
Romain Grandjean <romain.grandjean@outlook.com>
2021-11-30 17:45:07 +0100

    implementing browser version detetection & message error - testing

--- components/layout.js
+++ components/layout.js
@@ -1,14 +1,28 @@
 import styles from '../styles/components/Layout.module.scss';
+import { useState } from 'react';
+import { detect } from 'detect-browser';
 
 // Components
 import Header from '../components/header';
 import Footer from '../components/footer';
 
 export default function Layout({ children }) {
+	const browser = detect();
+	const [compatible, setCompatible] = useState(true);
+
+	if (browser.name === 'chrome' && browser.version < 56) {
+		setCompatible(false);
+	}
+
 	return (
 		<div className={styles.pageLayout}>
 			<Header />
-			<div className={styles.contentLayout}>{children}</div>
+			{!compatible && (
+				<h1 style={{ zIndex: 99 }}>
+					Your browser needs to be upgraded to use this wbesite
+				</h1>
+			)}
+			<content className={styles.contentLayout}>{children}</content>
 			<Footer />
 		</div>
 	);
