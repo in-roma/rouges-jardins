1d710e4096ff4738bd6e46f0ddb6e9bbec622129
Romain Grandjean <romain.grandjean@outlook.com>
2021-12-02 13:10:24 +0100

    font optmization

--- pages/_document.js
+++ pages/_document.js
@@ -11,8 +11,17 @@ class MyDocument extends Document {
 			<Html lang="fr">
 				<Head>
 					<link
+						rel="preconnect"
+						href="https://fonts.googleapis.com"
+					/>
+					<link
+						rel="preconnect"
+						href="https://fonts.gstatic.com"
+						crossorigin
+					/>
+					<link
+						href="https://fonts.googleapis.com/css2?family=Lora:wght@600;700&family=Poppins:wght@200;300;400;500;600;700;800&display=swap"
 						rel="stylesheet"
-						href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700&display=swap"
 					/>
 				</Head>
 				<body>
