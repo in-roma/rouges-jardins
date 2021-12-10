485b832c7f4a22746a5ec178188d313cf946c5f6
Romain Grandjean <romain.grandjean@outlook.com>
2021-12-10 22:39:15 +0100

    adding viewport meta disabling auto zoom safari

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,10 @@ class MyDocument extends Document {
 		return (
 			<Html lang="fr">
 				<Head>
+					<meta
+						name="viewport"
+						content="width=device-width, initial-scale=1, maximal-scale=1"
+					/>
 					<link
 						href="https://fonts.googleapis.com/css2?family=Lora:wght@600;700&family=Poppins:wght@200;300;400;500;600;700;800&display=swap"
 						rel="stylesheet"
